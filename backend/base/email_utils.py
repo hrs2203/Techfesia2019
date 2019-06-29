c697dcfdeb17ec4a02b957fcffe2739e5c6b2fc7
Hrishabh Pandey <44145550+hrs2203@users.noreply.github.com>
2019-06-29 23:36:17 +0530

    templated integration
    integration of templeates part1 completed

---
+++ backend/base/email_utils.py
@@ -0,0 +1,25 @@
+# will contain all email utils
+import os
+from django.core.mail import send_mail , EmailMultiAlternatives
+from django.conf import settings
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+
+# from_email = settings.EMAIL_HOST_USER
+
+# email_file_address = settings.BASE_DIR + '/base/email_templates'
+# email_file_address = os.path.join( settings.BASE_DIR , 'base' , 'email_templates' )
+
+def welcomeEmail( to_email , from_email = 'codehrs2203@gmail.com' ):
+    _subject = "welcome email"
+    _from = from_email
+    _to = to_email
+
+    # content will be a html file
+    content_message = render_to_string( 'welcome.html' , {})
+    text_message = strip_tags(content_message)
+
+    # send_mail( _subject , content_message , _from, _to, fail_silently=True)
+    send_mail = EmailMultiAlternatives( _subject , text_message , _from , _to )
+    send_mail.attach_alternative(content_message,'text/html')
+    send_mail.send()
